374d64f129ca239b1923b679cfe641f2fa2daf7c
Abir-Hossain <abirhossain9@iut-dhaka.edu>
2023-03-30 11:06:44 +0600

    Sets up order service

---
+++ src/main/java/apigateway/OrderServiceApplication.java
@@ -0,0 +1,20 @@
+package apigateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderServiceApplication {
+	// other code
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+}
